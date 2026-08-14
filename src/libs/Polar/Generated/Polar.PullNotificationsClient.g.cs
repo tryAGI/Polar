722f6226469c32527baf1242bbb551f30c36cf46
Konstantin S. <havendv@gmail.com>
2026-08-14 19:58:05 +0400

    feat: Updated OpenAPI spec (#27)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Polar/Generated/Polar.PullNotificationsClient.g.cs
+++ src/libs/Polar/Generated/Polar.PullNotificationsClient.g.cs
@@ -34,10 +34,17 @@ namespace Polar
 
         /// <inheritdoc/>
         public global::Polar.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Polar.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Polar.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
